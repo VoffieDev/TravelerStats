80f703d8236d75bd4461e30419d89d997ba6645a
VoffieDev <viktorsoderborg@gmail.com>
2022-12-01 17:17:47 +0100

    Add background img & fix styling

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import { Quasar } from "quasar";
+import router from "./router";
 import "./style.css";
 import App from "./App.vue";
-import router from "./router";
-import { Quasar } from "quasar";
+import "@quasar/extras/material-icons/material-icons.css";
+import "@quasar/extras/bootstrap-icons/bootstrap-icons.css";
+import "quasar/src/css/index.sass";
 
 createApp(App).use(router).use(Quasar).mount("#app");
